b2e00d32b2daba5c42780560d653e57e0f4fb198
Nattapon Jaroenchai <b_nj18@live.com>
2023-02-09 18:51:39 -0600

    Add Finished the home page and page template
    Copy over from Shivam repo and Nathan repo. For the design, we will work on this one from now on.

---
+++ assets/js/navigation.js
@@ -0,0 +1,62 @@
+$(function(){
+    $('#main-menu').slicknav({
+        label: '',
+        prependTo:'#navigation',
+        closedSymbol: '<span class="icon-chevron-right"></span>',
+        openedSymbol: '<span class="icon-chevron-down"></span>',
+    });
+    $('.slicknav_menu').prepend('<img class="m-2 h-[30px] w-auto inline-block" src="./assets/css/logo-color.png"alt="">');
+    $('.slicknav_menu').addClass('sticky top-0 z-50 block md:hidden bg-white text-black');
+
+    // Add the arrows to navigation items
+    $('#navigation #main-menu>li.menu-item-has-children > a').append('<span class="pl-5 pr-3 inline-block icon-angle-down"></span>');
+    $('#navigation #main-menu>li>ul.sub-menu>li.menu-item-has-children > a').prepend('<span class="pl-0 pr-3 inline-block icon-angle-left"></span>');
+
+});
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 450;
+var navbarHeight = $('#navigation').outerHeight();
+
+$(window).scroll(function (event) {
+    didScroll = true;
+});
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    // console.log(lastScrollTop, st, Math.abs(lastScrollTop - st), delta)
+    if (st == 0) {
+        $('#navigation').addClass('bg-transparent text-white').removeClass('bg-white text-black shadow-xl');
+    } else if (st > 0 && st < 400) {
+        $('#navigation').addClass('bg-white text-black shadow-xl').removeClass('bg-transparent text-white');
+    }
+
+    if (Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('#navigation').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height() + 200) {
+            $('#navigation').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./asset/**/*.{html,js}"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'ig-blue': '#0f5c98',
+        'ig-teal':  '#2fa9a3',
+        'ig-olive': '#63853a',
+        'ig-light-green': '#9ecda2', 
+        'ig-orange': '#f18149',
+        'ig-mustard': '#e7b52f', 
+        'ig-dark-blue': '#13284b'
+      },
+    },
+    fontFamily: {
+      sans: [
+        '"Inter"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+  },
+}
